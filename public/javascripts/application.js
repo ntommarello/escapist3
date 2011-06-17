7fa536fe6b4b46a16e0d6acb05b81be565114ad0
Nicholas Tommarello <nick@tommarello.net>
2011-06-17 04:14:09 -0400

    reorganize javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,9 @@
 
 
 $(document).ready(function() {
+	
+	$('#name').alpha({allow:"., "});
+	 
   $('input[title]').inputHints();
 	$(".disallowReturn").keypress(function(e) {
 		$(this).attr("edited","yes");
