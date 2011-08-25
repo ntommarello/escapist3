ed05641c7bceac4dc1867fd661d794d73c4e45ad
Nicholas Tommarello <nick@tommarello.net>
2011-08-25 15:00:37 -0400

    signup fix

--- app/controllers/subscribed_plans_controller.rb
+++ app/controllers/subscribed_plans_controller.rb
@@ -21,8 +21,8 @@ class SubscribedPlansController < ApplicationController
        
        @challenge_url = "#{@plan.id}-#{@plan.title.parameterize}"
        Postoffice.cc_comment(current_user.first_name, current_user.last_name, current_user.id, 
-                             @plan.users[0].email, "", 
-                             @plan.user.authentication_token, @plan.title,@challenge_url,"joined").deliver
+                             @plan.hosts[0].email, "", 
+                             @plan.hosts[0].authentication_token, @plan.title,@challenge_url,"joined").deliver
        
        
        
