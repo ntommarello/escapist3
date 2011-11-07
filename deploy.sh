d85c6e7296d92e9a97e46698b8e2b1966f21d547
Nicholas Tommarello <nick@tommarello.net>
2011-11-06 21:36:28 -0500

    hello

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 git add .
 echo "Added files to git"
-git commit -m "fast commit"
+git commit -m $1
 echo "Committed to git"
 git push
 echo "pushing to remote repo"
-RUBBER_ENV=production cap deploy:fast
+RUBBER_ENV=production cap deploy$2
 echo "DEPLOYED.  YAY!"
