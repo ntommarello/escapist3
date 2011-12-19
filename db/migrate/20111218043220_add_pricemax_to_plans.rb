69214a7e30277cb0e7915c475d447a7eb3d16d32
Nicholas Tommarello <nick@tommarello.net>
2011-12-18 21:47:19 -0500

    edittickets

--- db/migrate/20111218043220_add_pricemax_to_plans.rb
+++ db/migrate/20111218043220_add_pricemax_to_plans.rb
@@ -1,6 +1,6 @@
 class AddPricemaxToPlans < ActiveRecord::Migration
   def self.up
-    add_column :plans, :price_max, :decimal, :scale => 2
+    add_column :plans, :price_max, :decimal, :scale => 2, :precision=>10
   end
 
   def self.down
